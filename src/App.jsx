a62762057a61e31de0f8f53ee4cebf4d5471426b
cterryc <danyel.martel@gmail.com>
2024-03-30 21:30:06 -0500

    paginado 1

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,30 @@ import Species from "./components/Filtro/Species";
 import Gender from "./components/Filtro/Gender";
 import Status from "./components/Filtro/Status";
 
+const paginacion = 42;
+
+const s = ["rick", "morty", "summer"];
+
+function añadir(nombre) {
+  s.filter((ele) => ele === "rick");
+  s.shift("");
+}
+
+const botones = [];
+for (let i = 0; i < paginacion; i++) {
+  botones.push(i + 1);
+}
+
+console.log(botones);
+
 function App() {
   const [characters, setCharacters] = useState(null);
   const [fondo, setFondo] = useState(null);
   const [fondo2, setFondo2] = useState(null);
   const [fondo3, setFondo3] = useState(null);
   const [especies, setEspecies] = useState(null);
+  const [nombre, setNombre] = useState(null);
+  const [pagina, setPagina] = useState(1);
 
   useEffect(() => {
     const personajes = fetch("https://rickandmortyapi.com/api/character")
@@ -22,6 +40,23 @@ function App() {
         setCharacters(reponse.results);
       });
   }, []);
+
+  const paginacion = async (ele) => {
+    console.log(ele);
+    const getPagina = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${ele}`
+    );
+    const data = await getPagina.json();
+    setCharacters(data.results);
+    setPagina(ele);
+  };
+
+  // const url = async (numero) => {
+  //   const url = await fetch(`https://rickandmortyapi.com/api/character?page=${numero}`)
+  //   const data = await url.json();
+  //     setCharacters(data.results);
+  // }
+
   const Filtro = async (estado) => {
     const temporal = await fetch(
       `https://rickandmortyapi.com/api/character/?name=rick&status=${estado}`
@@ -49,6 +84,22 @@ function App() {
     const data = await temporal.json();
     setCharacters(data.results);
   };
+  const Buscador = async () => {
+    const temporal = await fetch(
+      `https://rickandmortyapi.com/api/character/?name=${nombre}`
+    );
+    const data = await temporal.json();
+    setCharacters(data.results);
+  };
+  const search = async (e) => {
+    const propiedad = e.target.value;
+    setNombre(propiedad);
+  };
+  const Teclado = (e) => {
+    if (e.key === "Enter") {
+      Buscador();
+    }
+  };
   return (
     <div className="app">
       <nav className="barra">
@@ -61,8 +112,16 @@ function App() {
       </nav>
       <h1 className="Characters">Characters</h1>
       <div className="Buscador">
-        <input type="text" placeholder="Buscar personajes" className="Input" />
-        <button className="Boton">Buscar</button>
+        <input
+          type="text"
+          onChange={search}
+          placeholder="Buscar personajes"
+          className="Input"
+          onKeyPress={Teclado}
+        />
+        <button className="Boton" onClick={Buscador}>
+          Añadir
+        </button>
       </div>
       <div className="Contenedor">
         <div className="Filter">
@@ -77,6 +136,22 @@ function App() {
               return <Personajes propiedades={characters && elemento} />;
             })}
         </div>
+      </div>
+      <div className="paginado">
+        <button onClick={() => pagina >= 2 && paginacion(pagina - 1)}>
+          Prev
+        </button>
+        {/* const botones = [1, 2, 3, 4, 5] */}
+        {botones.map((ele, index) => {
+          return (
+            <button onClick={() => paginacion(ele)} key={index}>
+              {ele}
+            </button>
+          );
+        })}
+        <button onClick={() => pagina <= 41 && paginacion(pagina + 1)}>
+          Next
+        </button>
       </div>
     </div>
   );
